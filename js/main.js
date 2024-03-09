b8650d0c2b807978f68358f19754d9935d5c713a
DianaPavlova <diana50888@gmail.com>
2024-03-09 11:18:24 +0200

    Добавляет порционную загрузку комментариев при открытии большой фотогорафии.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import {photosData} from './data';
 import {renderMiniGallery} from './mini-gallery';
-import {miniGalleryClick} from './big-photo';
+import './big-photo';
 
 renderMiniGallery(photosData);
-miniGalleryClick();
